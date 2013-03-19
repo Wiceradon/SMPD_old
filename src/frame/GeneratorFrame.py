11102ee2787397eed11c0560483c86c13aebe1b3
Jakub Jeleński <jjelenski90@gmail.com>
2013-03-19 06:32:52 +0100

    Rozbudowa GeneratorFrame oraz InputWrapper

--- src/frame/GeneratorFrame.py
+++ src/frame/GeneratorFrame.py
@@ -19,16 +19,108 @@ class GeneratorFrame:
         '''
         
         self.frame = frame
-        self.field = InputWrapper(self.frame, "Podaj int: ", self.validFloat)
-        self.field.frame.pack(side = TOP)
-        self.button = Button(self.frame, text = "Waliduj", command = self.field.validator)
-        self.button.pack(side = BOTTOM)
-    
-    def a(self):
-        self.field.validate  
+        self.globalInputs = []
+        self.globalSettingsFrame1 = Frame(self.frame)
+        self.globalSettingsFrame1.pack(side = TOP)
+        self.globalSettingsFrame2 = Frame(self.frame)
+        self.globalSettingsFrame2.pack(side = LEFT)
+        self.validationError = False
+        self.generator = Generator()
         
-    def validInt(self, value):
-        print value.isdigit()
+        self.errorString = StringVar()
+        Label(self.frame, textvariable = self.errorString, fg = 'red').pack(side = BOTTOM)
+        
+        field = InputWrapper(self.globalSettingsFrame1, "Podaj globalne mu: ", self.validFloat, self.returnFloat)
+        field.frame.pack(side = LEFT)
+        self.globalInputs.append(field)
+        
+        field = InputWrapper(self.globalSettingsFrame1, "Podaj globalne sigma: ", self.validFloat, self.returnFloat)
+        field.frame.pack(side = LEFT)
+        self.globalInputs.append(field)
+        
+        field = InputWrapper(self.globalSettingsFrame1, "Podaj globalna liczebnosc grup: ", self.validPositiveInt, self.returnInt)
+        field.frame.pack(side = LEFT)
+        self.globalInputs.append(field)
+        
+        field = InputWrapper(self.globalSettingsFrame2, "Podaj liczbe grup: ", self.validPositiveInt, self.returnInt)
+        field.frame.pack(side = LEFT)
+        self.globalInputs.append(field)
+        field = InputWrapper(self.globalSettingsFrame2, "Podaj ilosc cech: ", self.validPositiveInt, self.returnInt)
+        field.frame.pack(side = LEFT)
+        self.globalInputs.append(field)
+        
+        self.button = Button(self.globalSettingsFrame2, text = "Waliduj", command = self.setGlobal)
+        self.button.pack(side = LEFT)
+    
+    def setGlobal(self):
+        self.validateGlobal()
+        if self.validationError == True: return
+        
+        self.generator.gloabalMu = self.globalInputs[0].returnFormatted()
+        self.generator.gloabalSigma = self.globalInputs[1].returnFormatted()
+        self.generator.gloabalClassSize = self.globalInputs[2].returnFormatted()
+        self.generator.numberOfClasses = self.globalInputs[3].returnFormatted()
+        self.generator.featureVectorLength = self.globalInputs[4].returnFormatted()
+        for i in self.globalInputs:
+            i.dataField.configure(state="disable")
+        self.showClassInput()
+    
+    def validateGlobal(self):
+        self.validationError = False
+        for i in self.globalInputs:
+            if i.validate() == False:
+                self.errorString.set("Wystapil problem walidacji pola: '"+i.labelText+"'. Wartosc '"+i.dataField.get()+"' posiada zly format.")
+                self.validationError = True
+                break
+        
+    def validPositiveInt(self, value):
+        if value == "": return True
+        return value.isdigit() and (int(value)>0)
     
     def validFloat(self, value):
-        print float(value)
+        if value == "": return True
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
+        
+    def returnInt(self, value):
+        if value == "": return None
+        return int(value)
+    
+    def returnFloat(self, value):
+        if value == "": return None
+        return float(value)
+    
+    def returnString(self, value):
+        return value
+    
+    def showClassInput(self):
+        self.classFrame = Frame(self.frame)
+        self.classCount = StringVar()
+        self.classCount.set("Utworzono: 0")
+        Label(self.classFrame, textvariable = self.classCount).pack(side = LEFT)
+        self.classSize = InputWrapper(self.classFrame, "Liczebnosc grupy:", 
+                                      self.validPositiveInt, self.returnInt)
+        self.classSize.frame.pack(side = LEFT)
+        self.addClassButton = Button(self.classFrame, text = "Dodaj grupe", command = self.performAddClass, state = 'disabled')
+        self.addClassButton.pack(side = LEFT)
+        self.featureFrame = Frame(self.classFrame)
+        self.featureFrame.pack(side = BOTTOM)
+        self.featureCount = StringVar()
+        self.featureCount.set("Utworzono cech: 0")
+        Label(self.featureFrame, textvariable = self.featureCount).pack(side = LEFT)
+        self.classMu = InputWrapper(self.featureFrame, "Mu:", self.validPositiveInt, self.returnInt)
+        self.classMu.frame.pack(side = LEFT)
+        self.classSigma = InputWrapper(self.featureFrame, "Sigma:", self.validPositiveInt, self.returnInt)
+        self.classSigma.frame.pack(side = LEFT)
+        self.addFeatureButton = Button(self.featureFrame, "Dodaj ceche", command = self.performAddFeature)
+        self.addFeatureButton.pack(side = LEFT)
+        self.classFrame.pack(side = TOP)
+    
+    def performAddFeature(self): 
+        print "A"
+        
+    def performAddClass(self):
+        print "P"
--- src/widgets/InputWrapper.py
+++ src/widgets/InputWrapper.py
@@ -11,16 +11,21 @@ class InputWrapper:
     '''
 
 
-    def __init__(self, parent, labelText, validateCondition):
+    def __init__(self, parent, labelText, validateCondition, format):
         '''
         Constructor
         '''
         self.frame = Frame(parent)
+        self.labelText = labelText
         self.labelField = Label(self.frame, text = labelText)
         self.labelField.pack(side = LEFT)
         self.dataField = Entry(self.frame)
         self.dataField.pack(side = LEFT)
-        self.validator = lambda : self.executeValidator(validateCondition)
+        self.validate = lambda : self.executeValidator(validateCondition)
+        self.returnFormatted = lambda : self.formatValue(format)
     
     def executeValidator(self, cond):
-        cond(self.dataField.get())
+        return cond(self.dataField.get())
+        
+    def formatValue(self, format):
+        return format(self.dataField.get())
